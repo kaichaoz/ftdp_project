81713e73a0d00198b6d38ff94f0cda85a3848283
曹轩 <15932641664@163.com>
2020-04-21 09:39:17 +0800

    优化停用启用组件的代码顺序和code、message返回值变成0000

--- src/main/java/com/tfjybj/ftdp/provider/service/impl/ComponentlibraryServiceImpl.java
+++ src/main/java/com/tfjybj/ftdp/provider/service/impl/ComponentlibraryServiceImpl.java
@@ -38,11 +38,11 @@ public class ComponentlibraryServiceImpl implements ComponentlibraryService {
             //如果接收到的IsUsable是0
             if (componentModels.getIsUsable().equals("0")) {
                 //将启用变成停用
-                componentlibraryDao.updateComponentIsUsableOff(componentModels.getComponentId());
+                componentlibraryDao.updateComponentIsUsableOpen(componentModels.getComponentId());
                 //如果IsUsable是1
             } else if (componentModels.getIsUsable().equals("1")) {
                 //将停用变成启用
-                componentlibraryDao.updateComponentIsUsableOpen(componentModels.getComponentId());
+                componentlibraryDao.updateComponentIsUsableOff(componentModels.getComponentId());
             }
         }catch (Exception e){
             e.printStackTrace();
