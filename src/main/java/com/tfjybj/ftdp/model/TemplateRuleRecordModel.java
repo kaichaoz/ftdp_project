a899aa55e7d00f0bf9c4bc6f43f84c7ad25a460f
Zachary <40531403+TGB-Duke@users.noreply.github.com>
2020-04-29 15:02:48 +0800

    撤销文件注释——张凯超——2020年4月29日14:56:02

--- src/main/java/com/tfjybj/ftdp/model/TemplateRuleRecordModel.java
+++ src/main/java/com/tfjybj/ftdp/model/TemplateRuleRecordModel.java
@@ -22,17 +22,17 @@ import java.io.Serializable;
 @Data
 public class TemplateRuleRecordModel implements Serializable{
 
-//    public String getId() {
-//        return id;
-//    }
-//
-//    public void setId(String id) {
-//        this.id = id;
-//    }
+    public String getId() {
+        return id;
+    }
 
-//    @Id
-//    @Column(name = "id")
-//    private String  id;
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Id
+    @Column(name = "id")
+    private String  id;
     @Column(name = "templateId")
     private String  templateId;
     @Column(name = "templateContentId")
