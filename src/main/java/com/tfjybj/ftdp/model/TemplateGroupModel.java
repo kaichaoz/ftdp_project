7749bea3f85589c45222edee630d2ca5c9eca70c
Zachary <40531403+TGB-Duke@users.noreply.github.com>
2020-04-30 10:13:38 +0800

    添加extends BaseEntity ——张凯超——2020年4月30日10:13:22

--- src/main/java/com/tfjybj/ftdp/model/TemplateGroupModel.java
+++ src/main/java/com/tfjybj/ftdp/model/TemplateGroupModel.java
@@ -1,9 +1,12 @@
 package com.tfjybj.ftdp.model;
 
+import com.tfjybj.ftdp.entity.BaseEntity;
 import lombok.Data;
+import lombok.NonNull;
 
 import javax.persistence.Column;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 /**
@@ -14,7 +17,7 @@ import java.io.Serializable;
  * @Version 1.0
  */
 @Data
-public class TemplateGroupModel implements Serializable{
+public class TemplateGroupModel extends BaseEntity implements Serializable{
     private String id;
     private String templateGroupName;
     private Integer isUsable;
