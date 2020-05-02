acace5ffba88bb25ac86cc549485b68c9b884568
chenhaiming <15732662145@163.com>
2020-05-02 16:40:31 +0800

    修改插入-陈海明-2020年4月29日

--- src/main/java/com/tfjybj/ftdp/provider/service/impl/TemplateRuleRecordServiceImpl.java
+++ src/main/java/com/tfjybj/ftdp/provider/service/impl/TemplateRuleRecordServiceImpl.java
@@ -67,7 +67,6 @@ public  class TemplateRuleRecordServiceImpl implements TemplateRuleRecordService
      * @Modification Time:
      **/
     public String addTemplateRuleRecord(TemplateRuleRecordModel templateRuleRecordModel) {
-
        templaterulerecordDao.addTemplateRuleRecord(templateRuleRecordModel);
         return templateRuleRecordModel.getId();
     }
